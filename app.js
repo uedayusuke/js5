cf5990734c6fdd137ca3d304a1f602e5fd738842
uedayusuke <uedayusuke1107@gmail.com>
2020-04-08 23:03:12 +0900

    [Finish]6-5

---
+++ app.js
@@ -0,0 +1,23 @@
+$(function(){
+	$(".box1").css({
+		"background-color": "#0000FF",
+		"height": "100px"
+	});
+});
+
+$(function(){
+	$(".box1").slideDown();
+	$(".box1").css({"background-color": "#0000FF"});
+});
+
+$(function(){
+	$(".box2").slideUp();
+});
+
+$(function(){
+	$(".box3").show();
+});
+
+$(function(){
+	$(".box4").hide();
+});
---
+++ test.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(".box1").slideDown(function(){
+		$(".box1").css({
+			"backgorund-color": "#0000FF",
+			"height": "100px",
+			"width": "200px"
+		}).slideUp();
+	});
+});
+
